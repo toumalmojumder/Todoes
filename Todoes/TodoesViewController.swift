4c7cb877703b94b51efa4aa7094ac89e2b908859
toumalmojumder <toumalmojumder@gmail.com>
2019-02-08 11:53:28 -0800

    chk cell
    deselect animation Added
    cell checkmark added

--- Todoes/TodoesViewController.swift
+++ Todoes/TodoesViewController.swift
@@ -16,6 +16,7 @@ class TodoesViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    //table view showing array data
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -23,6 +24,20 @@ class TodoesViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
+    }//
+    
+    // Table view delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 }
